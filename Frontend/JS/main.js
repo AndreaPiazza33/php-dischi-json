c3fd39bb3c2bb35b9b508d7b05c041f8f533c970
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-09-28 17:13:30 +0200

    fix:fixed api call

--- Frontend/JS/main.js
+++ Frontend/JS/main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      title: "",
+      discList: [],
     };
+  },
+  methods: {
+    fetchDiscList() {
+      axios
+        .get("http://localhost/php-dischi-json/Backend/Api/get-discs.php")
+        .then((result) => {
+          this.discList = result.data;
+          console.log(this.discList);
+        });
+    },
+  },
+  mounted() {
+    this.fetchDiscList();
   },
 }).mount("#app");
